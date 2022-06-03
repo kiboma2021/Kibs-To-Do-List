66641aa31970f6161e1eeeae5a3535a14a27a3d4
Ben Kiboma <omayioben@gmail.com>
2022-06-04 02:53:52 +0300

    Update local storage

--- src/index.js
+++ src/index.js
@@ -166,11 +166,11 @@ window.addEventListener('load',getFromLocal);
 const updateLocal = () => {
   const localData = JSON.parse(localStorage.getItem('List'));
   const todos = document.querySelectorAll(".description");
-  for (let i=1; i<todos.length; i++) {
+  for (let i=0; i<todos.length; i++) {
     if(todos[i].classList.contains('check-to-do')) {
-      localData[i].complited == true;
+      localData[i].complited = true;
     } else {
-      localData[i].complited == false;
+      localData[i].complited = false;
     }
   }
   localStorage.setItem('List',JSON.stringify(localData));
