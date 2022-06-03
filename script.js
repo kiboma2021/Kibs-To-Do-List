d1c6e96e8c9c39f0be75e1a73d91960f51adc631
Ben Kiboma <omayioben@gmail.com>
2022-06-03 12:18:57 +0300

    Edit input

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ const addTodo = toDovalue => {
   const checkbox = document.querySelectorAll('.checkbox');
   checkbox.forEach(i => {
     i.addEventListener('click', () => {
-      //i.parentElement.classList.toggle('checkedContainer');
+      i.parentElement.classList.toggle('checkedContainer');
       i.nextElementSibling.classList.toggle('check-to-do');
       i.parentElement.lastElementChild.classList.toggle('trash-active');
       i.parentElement.lastElementChild.previousElementSibling.classList.toggle('edit-disabled');
@@ -42,7 +42,7 @@ const addTodo = toDovalue => {
   const editIcons = document.querySelectorAll(".fa-ellipsis-v");
   editIcons.forEach (i => {
     i.addEventListener ('click', ()=> {
-      editTodo();
+      editTodo(toDocontainer, i.previousElementSibling);
     })
   })
 };
@@ -53,7 +53,24 @@ const editTodo = (toDocontainer, todo) => {
   editInput.type = "text";
   editInput.className = "editInput";
   editInput.value = todo.textContent;
+  toDocontainer.replaceChild(editInput, todo);
 
+  //add event listerner to edit input
+  editInput.addEventListener ('keypress', e => {
+    if (e.key === 'Enter') {
+      const editContainers = document.querySelectorAll('.toDocontainer');
+      const localData = JSON.parse (localStorage.getItem('List'));
+      for (let i=1; i<editContainers.length; i++) {
+        if (editContainers[i].classList.contains('checkedContainer')) {
+          localData[i].description = editInput.value;
+          localStorage.setItem ('List', JSON.stringify(localData));
+        }
+      }
+      editInput.parentElement.classList.remove('checkedContainer');
+      toDocontainer.replaceChild(todo, editInput);
+      todo.textContent = editInput.value;
+    }
+  })
 }
 
 //Add event lister when enter is clicked while in input field
