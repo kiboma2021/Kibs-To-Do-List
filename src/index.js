4522569941d2cf57a980e936f3b846b13fb7f17a
Ben Kiboma <omayioben@gmail.com>
2022-06-12 15:37:02 +0300

    Style header

--- src/index.js
+++ src/index.js
@@ -177,3 +177,13 @@ const clearAll = () => {
 }
 
 clearContent.addEventListener ('click', clearAll);
+
+
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = dd + '/' + mm + '/' + yyyy;
+document.getElementById("date").innerHTML = today;
